d2dbf51ae9bd6dd4af8839f3cc5271fa9c4c9e7f
moriyamaekj <32349051+moriyamaekj@users.noreply.github.com>
2026-02-19 12:39:34 +0900

    add tj002 setting files
    tj002 foida wo tuika
    tj-002no file wo sakusei

---
+++ keyboards/ekjapan/tj002/keymaps/default/keymap.c
@@ -0,0 +1,28 @@
+// Copyright 2023 QMK
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#include QMK_KEYBOARD_H
+
+#include "keymap_japanese.h"
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+  [0] = LAYOUT(
+  //,-----------------------------------|
+                 KC_F2,  JP_EQL,
+  //|--------+--------+--------+--------|
+        KC_P7,   KC_P8,   KC_P9, KC_PSLS,
+  //|--------+--------+--------+--------|
+        KC_P4,   KC_P5,   KC_P6, KC_PAST,
+  //|--------+--------+--------+--------|
+        KC_P1,   KC_P2,   KC_P3, KC_PMNS,
+  //|--------+--------+--------+--------|
+        KC_P0, KC_PDOT, KC_PENT, KC_PPLS
+  //|-----------------------------------|
+  )
+};
+
+#if defined(ENCODER_MAP_ENABLE)
+const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][2] = {
+    [0] =   { ENCODER_CCW_CW(KC_MS_WH_DOWN, KC_MS_WH_UP), ENCODER_CCW_CW(KC_VOLD, KC_VOLU)  },
+};
+#endif
